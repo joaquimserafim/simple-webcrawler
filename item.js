5c2eeaa72e1f38d9fbd3b8f9291325048d165437
quim <joaquim.serafim@gmail.com>
2013-07-30 16:04:13 +0000

    Update item.js

--- item.js
+++ item.js
@@ -1,24 +1,13 @@
-/*global require*/
-/*blobal exports*/
-/**
- * Created with IntelliJ IDEA.
- * User: joaquimserafim
- * Date: 11/05/13
- * Time: 16:36
- * To change this template use File | Settings | File Templates.
- */
 /**
  * external modules
  */
 var mongojs = require('mongojs');// mongojs driver
-/**
- * internal
- */
-var mongodbClient = mongojs('mongodb://localhost:27017/crawler?w=1', ['items']);// mongodb client
-/**
- * Item class
- */
-var Item = function (obj) {
+
+// mongodb client
+var mongodbClient = mongojs('mongodb://localhost:27017/crawler?w=1', ['items']);
+
+
+function Item(obj) {
 	for (var key in obj) {// iterate keys in the object passed
 		if (obj.hasOwnProperty(key)) {
 			this[key] = obj[key];// merge values
@@ -29,10 +18,10 @@ var Item = function (obj) {
  * save item
  * @param cb - callback
  */
-Item.prototype.save = function (cb) {
+Item.prototype.save = function(cb) {
 	var self = this;
 	// save doc in MongoDB
-	mongodbClient.items.insert(self, {safe: true}, function (err, res) {
+	mongodbClient.items.insert(self, {safe: true}, function(err, res) {
 		if (err) {
 			return cb(err, null);
 		}
